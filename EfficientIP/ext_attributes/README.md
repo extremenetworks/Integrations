45e08478430f1ce00885c7b693fe276019704729
Stephan Harrer <47830551+StephanHarrer@users.noreply.github.com>
2019-09-04 19:36:09 +0200

    EfficientIP Integration for ext_attributes added (#1)
    * Create AddXMCNotification.png
    
    * Create AdminPage.png
    
    * Create ClassXMC.png
    
    * Update README.md
    
    * Create README.md
    
    * Create XMCNotificationContent.png
    
    * Create README.md
    
    * Create NewTextArea.png
    
    * Create NewClass.png
    
    * Create FilterForXMC.png
    
    * Create EIP_Example2.png
    
    * Create EIP_Example1.png
    
    * Create CreateTextArea.png
    
    * Create CreateClass.png
    
    * Create ConfigXMCNotification.png
    
    * Create AdminPageMarked.png
    
    * Update README.md
    
    * Update README.md
    
    * Create XMC2EIP.py
    
    * Update XMC2EIP.py
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

---
+++ EfficientIP/ext_attributes/README.md
@@ -0,0 +1,101 @@
+# Information enhancement
+
+>Powered by S. Harrer [Bell Computer-Netzwerke GmbH](http://www.bell.de/) based on Zdenek Palas Infoblox integration.
+
+## Extreme Management Center and EfficientIP (EIP) configuration
+
+### Prepare EfficientIP extensible attributes when using EIP version 7.1 and newer
+* Go to EfficientIP-GUI
+* Go to "Admin Home"
+
+![AdminPage](AdminPage.png)
+
+* Select "Class Studio" in "Customization"
+
+![AdminPageMarked](AdminPageMarked.png)
+
+* Create a new custom class
+
+![NewClass](NewClass.png)
+
+* Configure and enable the new custom class
+
+![CreateClass](CreateClass.png)
+
+* Filter for the newly created class and click on the name to edit the class
+
+![FilterForXMC](FilterForXMC.png)
+
+* Add a TEXT AREA for the XMC Status
+
+![CreateTextArea](CreateTextArea.png)
+
+![NewTextArea](NewTextArea.png)
+
+* Repead the previous step (= add a TEXT AREA) for the following information elements
+	
+	| Name     			| Label         	 | 
+	|:-----------------	|:------------------ | 
+	| xmcauthtype 		| XMC Authtype  	 | 
+	| xmcswitchip 		| XMC SwitchIP    	 | 
+	| xmcswitchport 	| XMC Switch Port    |  
+	| xmcswitchlocation | XMC Switch Location|
+	| xmcprofile		| XMC Profile 		 |
+	| xmcusername 		| XMC Username		 |
+	| xmcreason			| XMC Reason		 |
+	| xmcupdate			| XMC Update		 |
+
+* If you did the mentioned steps your class should look like the following picture
+
+![ClassXMC](ClassXMC.png)
+  
+  
+### Install the script
+* Download the [XMC2EIP.py](XMC2EIP.py?raw=true)
+* Update the variables in the XMC2EIP.py according your needs (Username,Password and IP Address)
+```bash
+varUser = "ipmadmin"
+varPassword = "admin"
+varEipIP = '1.1.1.1'
+varDebug = True
+varDebugFile = "XMC2EIP.log"
+```
+* Upload the `XMC2EIP.py` script to the Extreme Management Center to `/usr/local/bin`.
+* Change the script to be executable:
+```bash
+chmod +x /usr/local/bin/XMC2EIP.py
+```
+
+### Setup notifications (execution of API calls) with XMC version 8.0 and newer
+* Click "Add" in XMC Access Control Configuration -> Notifications
+
+![AddXMCNotification](AddXMCNotification.png)
+
+* Configure Notification
+
+![ConfigXMCNotification](ConfigXMCNotification.png)
+
+Program:	`/usr/local/bin/XMC2EIP.py`
+Working Directory:	`/tmp`
+Override Content:	`checked`
+Edit Content `…`
+
+![XMCNotificationContent](XMCNotificationContent.png)
+
+Custom Arguments:
+`Mac $macAddress Status $state Auth $authType SwitchIP $switchIP SwitchPort $switchPortId SwitchLocation $switchLocation Profile $nacProfileName User $username Reason $reason Time $lastSeenTime`
+
+## Example and usage
+### Extensible attributes in IPAM
+* Extensible attributes are stored in EfficientIP database only for Fixed Address / Reservation
+![EIP_Example1](EIP_Example1.png)
+* Extensible attributes can be used in EfficientIP filters
+![EIP_Example2](EIP_Example2.png)
+
+
+# Support
+_The software is provided as-is and neither [Extreme Networks](http://www.extremenetworks.com/) nor [Bell Computer-Netzwerke](http://bell.de) has no obligation to provide maintenance, support, updates, enhancements, or modifications. Any support provided by [Extreme Networks](http://www.extremenetworks.com/) or [Bell Computer-Netzwerke](http://bell.de) is at its sole discretion._
+
+Issues and/or bug fixes may be reported on [The Hub](https://community.extremenetworks.com/extreme).
+
+>Be Extreme
---
+++ EfficientIP/ext_attributes/XMC2EIP.py
@@ -0,0 +1,221 @@
+#!/usr/bin/python
+##############################################################################
+#
+# Softwarename : XMC2EIP.py
+# Last Updated : August 20, 2019
+#
+# Purpose:  Send XMC data to EfficientIP IPAM
+#
+# Changes: Created
+#
+##############################################################################
+
+#############
+## IMPORTS ##
+#############
+
+import requests
+import re
+import sys
+import logging
+import base64
+
+###############
+## User data ##
+###############
+
+varUser = "ipmadmin"
+varPassword = "admin"
+varEipIP = '1.1.1.1'
+#Set varDebug to "True" if you need more debug information
+varDebug = False
+varDebugFile = "XMC2EIP.log"
+
+###################################
+## Custom tuning above not below ##
+###################################
+
+varAttrStatus = "xmcstatus"
+varAttrAuth = "xmcauthtype"
+varAttrSwitchIP = "xmcswitchip"
+varAttrSwitchPort = "xmcswitchport"
+varAttrSwitchLocation = "xmcswitchlocation"
+varAttrProfile = "xmcprofile"
+varAttrUser = "xmcuser"
+varAttrReason = "xmcreason"
+varAttrTimeStamp = "xmcupdate"
+
+##################################################
+## Values above must match those in EfficientIP ##
+##################################################
+
+if varDebug:
+    logging.basicConfig(filename=varDebugFile,level=logging.DEBUG)
+
+varMakeUpdate = False
+
+args = {}
+i = 1
+while i < len(sys.argv):
+    args[sys.argv[i]] = sys.argv[i+1]
+    i += 2
+if varDebug:
+    logging.debug("Attributes received:"+str(sys.argv[1:]))
+    logging.debug("Loaded attributes:"+str(args))
+
+#################################################################
+## Check arguments and prepare Extensible Attributes Structure ##
+#################################################################
+
+varExtensibleAttr = {}
+
+if "Mac" in args.keys():
+    varMAC = str(args["Mac"])
+else:
+    if varDebug:
+        logging.error("Missing mandatory argument Mac followed by value.")
+    print ("Missing mandatory argument Mac followed by value.")
+    sys.exit(1)
+
+if "Status" in args.keys():
+    varExtensibleAttr.update({varAttrStatus:{"value":args["Status"]}})
+    varMakeUpdate = True
+
+if "Auth" in args.keys():
+    varExtensibleAttr.update({varAttrAuth:{"value":args["Auth"]}})
+    varMakeUpdate = True
+    
+if "SwitchIP" in args.keys():
+    varExtensibleAttr.update({varAttrSwitchIP:{"value":args["SwitchIP"]}})
+    varMakeUpdate = True
+
+if "SwitchPort" in args.keys():
+    varExtensibleAttr.update({varAttrSwitchPort:{"value":args["SwitchPort"].replace('"',"")}})
+    varMakeUpdate = True
+
+if "SwitchLocation" in args.keys():
+    if args["SwitchLocation"].replace('"',"") == ' ':
+        varExtensibleAttr.update({varAttrSwitchLocation:{"value":'-'}})
+    else:
+        varExtensibleAttr.update({varAttrSwitchLocation:{"value":args["SwitchLocation"].replace('"',"")}})
+    varMakeUpdate = True
+    
+if "Profile" in args.keys():
+    varExtensibleAttr.update({varAttrProfile:{"value":args["Profile"].replace('"',"")}})
+    varMakeUpdate = True
+
+if "User" in args.keys():
+    if args["User"].replace('"',"") == ' ':
+        varExtensibleAttr.update({varAttrUser:{"value":str(args["Mac"])}})
+    else:
+        varExtensibleAttr.update({varAttrUser:{"value":args["User"].replace('"',"")}})
+    varMakeUpdate = True
+
+if "Reason" in args.keys():
+    varExtensibleAttr.update({varAttrReason:{"value":args["Reason"].replace('"',"")}})
+    varMakeUpdate = True
+
+if "Time" in args.keys():
+    varExtensibleAttr.update({varAttrTimeStamp:{"value":args["Time"]}})
+    varMakeUpdate = True
+
+if varDebug:
+    logging.debug("Attributes in the structure: "+str(varExtensibleAttr))
+    logging.debug("End-System:"+str(args["Mac"]))
+
+if varMakeUpdate == False:
+    logging.error("At least one argument followed by value is needed: Status, Auth, SwitchIP, SwitchPort, SwitchLocation, Profile, User, Reason, Time")
+    print ("At least one argument followed by value is needed: Status, Auth, SwitchIP, SwitchPort, SwitchLocation, Profile, User, Reason, Time")
+    sys.exit(1)
+
+########################
+## Test if MAC exists ##
+########################
+
+headers = {
+	'X-IPM-Username': base64.standard_b64encode(varUser),
+	'X-IPM-Password': base64.standard_b64encode(varPassword),
+	'cache-control': 'no cache'}
+	
+url = "https://" + varEipIP + "/rest/ip_address_list"
+querystring = {"WHERE":"mac_addr like '" + varMAC + "'"}
+
+if varDebug:
+    logging.debug("requesting: " + url + " with querysting: " + str(querystring))
+
+try:
+	varResponse = requests.request("GET", url, headers=headers, params=querystring, verify=False)
+except requests.exceptions.RequestException as e:
+    logging.error(str(e))
+    sys.exit("RequestException")
+	
+if varDebug:
+    logging.debug("response: " + varResponse.text)
+	
+if varResponse.raise_for_status():
+	logging.error("ERROR communicating to EIP " + str(varResponse.status_code) + varResponse.text)
+	sys.exit("ERROR communicating to EIP")
+		
+varSearchResult = re.search(r"ip_id\":\"(\d*)", varResponse.text)
+
+if varSearchResult:
+	varIpId = varSearchResult.group(1)
+	print("IP_ID: " + varIpId)
+else:
+	logging.error("Can't find MAC-Address " + varMAC)
+	sys.exit("UnknownMAC")
+
+if (re.search(r"dhcphost_id\":\"0\",\"dhcplease_id\":\"[1-9]+", varResponse.text)):
+	sys.exit("DynamicAddress")
+
+varIsStatic = False
+
+if (re.search(r"dhcphost_id\":\"[1-9]+\",\"dhcplease_id\":\"\d+", varResponse.text)):
+	varIsStatic = True
+	
+varSearchResult = re.search(r"site_id\":\"(\d*)", varResponse.text)
+
+if varSearchResult:
+	varSiteId = varSearchResult.group(1)
+	print("Site_ID: " + varSiteId)
+else:
+	logging.error("Can't find Site_ID")
+	sys.exit("WrongSiteID")
+
+class_param = ""
+varParamCounter = 0
+
+for key in varExtensibleAttr:
+	if varParamCounter == 0:
+		class_param = class_param + key + "=" + varExtensibleAttr[key]['value']
+	else:
+		class_param = class_param + "&" + key + "=" + varExtensibleAttr[key]['value']
+	varParamCounter = 1
+
+if varIsStatic:
+	class_param = class_param + "&dhcpstatic=1"
+	querystring = {"ip_id":varIpId,"site_id":varSiteId,"mac_addr":varMAC,"ip_class_parameters":class_param}
+else:
+	querystring = {"ip_id":varIpId,"site_id":varSiteId,"ip_class_parameters":class_param}
+	
+url = "https://" + varEipIP + "/rest/ip_add"
+
+if varDebug:
+    logging.debug("requesting: " + url)
+	
+try:
+	varResponse = requests.request("PUT", url, headers=headers, params = querystring, verify=False)
+except requests.exceptions.RequestException as e:
+    logging.error(str(e))
+    sys.exit("RequestException")
+
+if varDebug:
+	logging.debug ("response:" + varResponse.text)
+
+if varResponse.raise_for_status():
+	logging.error("ERROR communicating to EIP "+str(varResponse.status_code) + varResponse.text)
+	sys.exit("ERROR communicating to EIP")
+
+#################
+## Be Extreme! ##
+#################
